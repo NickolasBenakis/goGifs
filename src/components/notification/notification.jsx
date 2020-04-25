2e1731f66b9be56bd8d208b49ac35f2e7569e323
nickolasBenakis <nickolasbenele@gmail.com>
2020-04-25 19:26:01 +0300

    Fix refs on notification

--- src/components/notification/notification.jsx
+++ src/components/notification/notification.jsx
@@ -2,19 +2,22 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Notification = ({ text, type }) => {
+	const containerRef = React.useRef(null);
 	const notRef = React.useRef(null);
 	const notificationClass = classNames('notification is-light', [type]);
-	const onClose = () => notRef.current.parentNode.removeChild(notRef.current);
+	const onClose = () => containerRef.current.removeChild(notRef.current);
 	return (
-		<div className={notificationClass} ref={notRef}>
-			<button
-				type='button'
-				onClick={onClose}
-				className='delete'
-				aria-label='close'
-			/>
-			{text}
-		</div>
+		<span ref={containerRef}>
+			<div className={notificationClass} ref={notRef}>
+				<button
+					type='button'
+					onClick={onClose}
+					className='delete'
+					aria-label='close'
+				/>
+				{text}
+			</div>
+		</span>
 	);
 };
 
